080b2adee6f3602d841d55d31ae78109eeb84d1a
sergey.sokolov <sergey.sokolov@behavox.com>
2022-02-11 15:00:38 -0500

    Updated packages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
 
 const Home = React.lazy(() => import('./Home'))
 
 const App = () => (
     <React.Suspense fallback={<>...</>}>
-        <Switch>
-            <Route exact={true} path="/" component={Home}/>
-        </Switch>
+        <Routes>
+            <Route path="/" element={<Home></Home>}/>
+        </Routes>
     </React.Suspense>
 );
 
